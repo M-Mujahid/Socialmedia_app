b57bbd47087210cafcbaf1b952afc984d36b6691
M-Mujahid <mujahidbuland@gmail.com>
2023-06-13 05:05:08 +0500

    signup

--- app.js
+++
@@ -1,6 +0,0 @@
-$(".input").focus(function(){
-        $(this).prev('.fa').addclass('glowIcon')
-    })
-    $(".input").focusout(function(){
-        $(this).prev('.fa').removeclass('glowIcon')
-    })
