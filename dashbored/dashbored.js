ea6b2385c8c7598951b2d08a3e90130075be7af1
M-Mujahid <mujahidbuland@gmail.com>
2023-06-15 23:44:48 +0500

    dashboared

--- dashbored/dashbored.js
+++ dashbored/dashbored.js
@@ -16,13 +16,9 @@ const firstName =
 const lastName = last.slice(0, 1).toUpperCase() + last.slice(1).toLowerCase();
 
 
-porfilePage.addEventListener('click',()=>{
-   window.location.href = "../profile/index.html"
-   
-})
-homePage.addEventListener('click',()=>{
-   window.location.href = "#"
-})
+// homePage.addEventListener('click',()=>{
+//    window.location.href = "#"
+// })
 
 logout.addEventListener("click", () => {
   window.location.href = "../index.html";
@@ -33,51 +29,37 @@ if (!isLoggedInUser) {
 }
 
 const objLeftDiv = [
-  { img: `profile.png`, text: `${firstName} ${lastName}` },
-  { img: "friemds.png", text: "Friends" },
-  { img: "recent.png", text: "Feeds (Most Recent)" },
+  { img: `avt-2-removebg-preview.png`, text: `${firstName} ${lastName}` },
+  { img: "avt-3-removebg-preview.png", text: "Friends" },
+  { img: "recent.png", text: "Feeds" },
   { img: "group.png", text: "Groups" },
-  { img: "market.png", text: "Marketplace" },
+  { img: "marketplace.png", text: "Marketplace" },
   { img: "watch.png", text: "Watch" },
-  { img: "memories.png", text: "Memories" },
-  { img: "saved.png", text: "Saved" },
-  { img: "pages.png", text: "Pages" },
-  { img: "addCenter.png", text: "Ad Center" },
-  { img: "graph.png", text: "Ads Manager" },
+  { img: "memmory.png", text: "Memories" },
+  { img: "save.png", text: "Saved" },
+
 ];
 
 const leftData = objLeftDiv.map((item) => {
   return `  <div class="itemImgName">
-    <img src="./assset/${item.img}" alt="" style="height: 40px; width: 40px; font-weight:bold;">
+    <img src="./assets/${item.img}"  style="height: 40px; width: 40px; font-weight:bold;">
     <p>${item.text}</p>
 </div>`;
 });
 leftDiv.innerHTML = leftData.join("");
 
 const objRightData = [
-  { img: `profile.png`, text: "Faryan Ahmed" },
-  { img: `profile.png`, text: "Muhammad Bilal" },
-  { img: `profile.png`, text: "Muhammad Shaban" },
-  { img: `profile.png`, text: "Muhammad shafeeq" },
-  { img: `profile.png`, text: "Muhammad Shayan" },
-  // { img: `profile.png`, text: "Imran Shaikh" },
-  // { img: `profile.png`, text: "Usama Ikram" },
-  // { img: `profile.png`, text: "Moiz Rasheed" },
-  // { img: `profile.png`, text: "Muhammad Saif" },
-  // { img: `profile.png`, text: "Ali Ahmy" },
-  // { img: `profile.png`, text: "Hammad Shah" },
-  // { img: `profile.png`, text: "Muhammad Ashhad" },
-  // { img: `profile.png`, text: "Muhammad Reyyan" },
-  // { img: `profile.png`, text: "Muhammad Nabeel" },
-  // { img: `profile.png`, text: "Ubaid Ur Rehman" },
-  // { img: `profile.png`, text: "Yasir Hussain" },
+  { img: `avt-2-removebg-preview.png`, text: "Umer Butt" },
+  { img: `avt-3-removebg-preview.png`, text: "Abdul Wahid" },
+  { img: `avt-2-removebg-preview.png`, text: "Muhammad Tahir" },
+  { img: `avt-3-removebg-preview.png`, text: "Aliyan Ghani" },
+  { img: `avt-2-removebg-preview.png`, text: "Shayan Ahmed" },
 ];
 
 const rightData = objRightData.map((item) => {
   return `<div class="itemImgName">
-      <img src="./assset/${item.img}" alt="" style="height: 40px; width: 40px; font-weight:bold;">
+      <img src="./assets/${item.img}" alt="" style="height: 40px; width: 40px; font-weight:bold;">
       <p>${item.text}</p>
-      <div class="onlineActive"></div>
   </div>`;
 });
 rightDiv.innerHTML = rightData.join("");
@@ -93,7 +75,7 @@ posts.forEach((post) => {
   div1.innerHTML = `<div class="postDivUpper">
   <div class="d-flex">
     <div class="postProfileImg">
-      <img src="./assset/profile.png" alt="" />
+      <img src="./assets/avt-2-removebg-preview.png" alt="" />
     </div>
     <div class="postNameDateTime">
       <h4 class="ProfileName">${post.userName}</h4>
@@ -102,38 +84,29 @@ posts.forEach((post) => {
     </div>
   </div>
   <div class="postDivEditDelete">
-    <img src="./assset/3Dots.png" alt="" />
-    <img src="./assset/cross.png" alt="" />
+    <img src="./assets/3Dots.png" alt="" />
+    <img src="./assets/cross.png" alt="" />
   </div>
 </div>
 <h6 class="m-3 text-white">${post.postContent}</h6>
 <div class="postDivImg">
-  <img class="postImg" src="./assset/postPic.jpg" alt="">
+  <img class="postImg" src="./assets/post.jpg" alt="">
 </div>
 <div class="likeLogoCounterComment d-flex justify-content-between">
   <div class="d-flex">
-    <img class="m-1" src="./assset/like.png" alt="" style="width: 20px ; height: 20px;" >
+    <img class="m-1" src="./assets/like.png" alt="" style="width: 20px ; height: 20px;" >
     <h6 class="text-white">1</h6>
-  </div>
-  <div class="d-flex text-white">
-    <h6 class="text-white"> 1 </h6>&ThinSpace;
-    <span>comment</span>&ThinSpace;
-    <span>.</span>&ThinSpace;
-    <h6>2</h6>&ThinSpace;
-    <span>reposts</span>
-
   </div>
 </div>
 <div class="d-flex justify-content-evenly text-white my-4 commentBox">
-  <div><img src="./assset/likeShow.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Like</div>
-  <div><img src="./assset/comment.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Comment</div>
-  <div><img src="./assset/repost.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Repost</div>
-  <div><img src="./assset/send.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Send</div>
+  <div><img src="./assets/like.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Like</div>
+  <div><img src="./assets/comment.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Comment</div>
+  <div><img src="./assets/share1.png" alt="" style="width: 30px ; height: 30px;">&ThinSpace;Share</div>
   
 </div>
 <div class="postCommentLastDiv">
-  <img src="./assset/profile.png" alt="" />
-  <input type="text" placeholder="Write a comment..." />
+  <img src="./assets/avt-2-removebg-preview.png"  />
+  <input type="text" placeholder="Leave a comment here..." />
   <button>Post</button>
 </div>
 `;
@@ -161,7 +134,7 @@ function postHandler() {
   div1.innerHTML = `<div class="postDivUpper">
   <div class="d-flex">
     <div class="postProfileImg">
-      <img src="./assets.3Dots.png />
+      <img src="./assets.3DFots.png />
     </div>
     <div class="postNameDateTime">
       <h4 class="ProfileName">${firstName} ${lastName}</h4>
@@ -209,7 +182,6 @@ postDiv.prepend(div1)
 
 const postObj = {
   userName: `${firstName} ${lastName}`,
-  userNumber: isLoggedInUser.mobileNum,
   postContent: placeholderName.value,
   date: new Date().toLocaleDateString(),
   time: new Date().toLocaleTimeString()
